5d98e92126065b6ec47d09f8df41e2d715b92075
gimi65536 <gimi65536@hotmail.com>
2018-06-30 12:53:12 +0800

    引入jjoojjoo123的stick串接
    利用呼叫big_anchor_concat_james來接收一個帶有word_set的字典。修改自jjoojjoo123/speechToText/findAllSticks.py當中的advance_score_calculating等函數，並且修改當中的資料存放細節
    並修改type hint的格式，現在引號只有後方有空格

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from itertools import combinations, product, chain
 from typing import List, Tuple, Any
 from collections import namedtuple
 from disjoint_set import disjoint_set
+from copy import deepcopy
 
 AnchorType = np.ndarray
 ScoreType = Any #float, int, etc. #Ord Scoretypr #C++ concept: LessThanComparable
@@ -104,10 +105,15 @@ class StringAlign():
 			return [self.__class__.join(i) for i in self._l[val]]
 		return self.__class__.join(self._l[val])
 	def copy(self):
+		"""
+		create a copy of object
+		the content of each list in _l is not allowed to be changed
+		, so it's no problem for shallow copy
+		"""
 		sol = type(self)()
-		sol._l = self._l.copy()
+		sol._l = self._l.copy() #
 		return sol
-	def evaluate(self, param : Param):
+	def evaluate(self, param: Param):
 		l = self._l
 		n = len(l)
 		state = self.__class__.State(n, dict())
@@ -116,7 +122,7 @@ class StringAlign():
 			#get = get[0], list(get[1])
 			state.Dict[(i, j)] = get
 		self._state = state
-	def big_anchor_concat_heuristic(self, param : Param, confidence : list = None):
+	def big_anchor_concat_heuristic(self, param: Param, confidence: list = None):
 		"""
 		provisional big-anchor function
 		"""
@@ -144,7 +150,310 @@ class StringAlign():
 		#this function should return one that contains word_set
 		#sets = list(word_set.sets())
 		#print(sentences_set) #all the sentences should become same
-	def print_big_anchor(self, word_set : disjoint_set = None):
+	def big_anchor_concat_james(self):
+		if self._state is None: #exception-like condition, maybe NoStateException
+			print('No state is ready!')
+			return
+		state = self._state
+		assert state.length == 4, "should be 4 sentences."
+		comb = sorted(state.Dict.keys(), key = (lambda i: state.Dict[i].similarity), reverse = True)
+		best_comb = comb[0]
+		another_comb = tuple(i for i in range(4) if i not in best_comb)
+		mapping = {j: i for i, j in enumerate([*best_comb, *another_comb])}
+		r_mapping = {i: j for i, j in enumerate([*best_comb, *another_comb])}
+		sticks = [[{0: i, 1: j} for i, j in state.Dict[best_comb].anchors], [{2: i, 3: j} for i, j in state.Dict[another_comb].anchors]]
+		after_change_sticks = [[{0: i, 1: j} for i, j in state.Dict[another_comb].anchors], [{2: i, 3: j} for i, j in state.Dict[best_comb].anchors]]
+		#word_set = disjoint_set.from_iterable(chain.from_iterable([(i, j) for j in range(len(self._l[i]))] for i in range(4)))
+		max_score_top_to_bottom, best_sticks_top_to_bottom = self.__class__._big_anchor_concat_james_helper([self._l[i] for i in best_comb] + [self._l[i] for i in another_comb], sticks)
+		max_score_bottom_to_top, best_sticks_bottom_to_top = self.__class__._big_anchor_concat_james_helper([self._l[i] for i in another_comb] + [self._l[i] for i in best_comb], after_change_sticks)
+		best_sticks_bottom_to_top = [[{(k+2)%4: i for k, i in d.items()} for d in best_sticks_bottom_to_top[1]], [{(k+2)%4: i for k, i in d.items()} for d in best_sticks_bottom_to_top[0]]]
+		best_sticks = [best_sticks_top_to_bottom[0] , best_sticks_bottom_to_top[1]]
+		for i in range(0, len(best_sticks[0])):
+			for j in range(0, len(best_sticks[1])):
+				if all(elem in best_sticks[1][j].items() for elem in best_sticks[0][i].items()):
+					best_sticks[0][i] = {}
+				elif all(elem in best_sticks[0][i].items() for elem in best_sticks[1][j].items()):
+					best_sticks[1][j] = {}
+				elif any(elem in best_sticks[0][i].items() for elem in best_sticks[1][j].items()):
+					if len(best_sticks[0][i]) >= len(best_sticks[1][j]):
+						best_sticks[1][j] = {}
+					else:
+						best_sticks[0][i] = {}
+		best_sticks[0] = list(filter(lambda a: len(a) > 0, best_sticks[0]))
+		best_sticks[1] = list(filter(lambda a: len(a) > 0, best_sticks[1]))
+
+		best_sticks = best_sticks[0] + best_sticks[1] #List[List[Dict]] -> List[Dict]
+
+		crosses = {}
+		for i in range(0,len(best_sticks)):
+			crosses[i] = []
+		for i in range(0, len(best_sticks) - 1):
+			for j in range(i+1, len(best_sticks)):
+				if len(best_sticks[i]) >= 3 and len(best_sticks[j]) >= 3:
+					order = []
+					find_cross = False
+					for i_word in best_sticks[i].items():
+						for j_word in best_sticks[j].items():
+							if i_word[0] ==  j_word[0]:
+								if len(order) == 0:
+									if i_word[1] > j_word[1]:
+										order = [j, i]
+									else:
+										order = [i, j]
+								else:
+									if i_word[1] > j_word[1] and order[0] == i:
+										crosses[i].append(j)
+										crosses[j].append(i)
+										find_cross = True
+										break
+									elif i_word[1] < j_word[1] and order[0] == j:
+										crosses[i].append(j)
+										crosses[j].append(i)
+										find_cross = True
+										break
+						if find_cross == True:
+							break
+		
+		# solve cross sticks
+		no_cross_sticks = []
+		while any(len(crosses[x]) > 0 for x in crosses):
+			max_len = 0
+			index_to_delete = -1
+			for i in range(0, len(crosses)):
+				if len(crosses[i]) > max_len:
+					index_to_delete = i
+					max_len = len(crosses[i])
+			
+			if len(best_sticks[index_to_delete]) == 4:
+				#no_cross_sticks.append([best_sticks[index_to_delete][0], best_sticks[index_to_delete][1]])
+				#no_cross_sticks.append([best_sticks[index_to_delete][2], best_sticks[index_to_delete][3]])
+				no_cross_sticks.append({0: best_sticks[index_to_delete][0], 1: best_sticks[index_to_delete][1]})
+				no_cross_sticks.append({2: best_sticks[index_to_delete][2], 3: best_sticks[index_to_delete][3]})
+				best_sticks[index_to_delete] = {}
+			
+			elif 0 in best_sticks[index_to_delete].keys() and 1 in best_sticks[index_to_delete].keys():
+				no_cross_sticks.append({0: best_sticks[index_to_delete][0], 1: best_sticks[index_to_delete][1]})
+				best_sticks[index_to_delete] = {}
+			
+			elif 2 in best_sticks[index_to_delete].keys() and 3 in best_sticks[index_to_delete].keys():
+				no_cross_sticks.append({2: best_sticks[index_to_delete][2], 3: best_sticks[index_to_delete][3]})
+				best_sticks[index_to_delete] = {}
+			
+			crosses[index_to_delete] = {}
+			for i in range(0, len(crosses)):
+				if index_to_delete in crosses[i]:
+					crosses[i].remove(index_to_delete)
+			
+		best_sticks = list(filter(lambda a: len(a) > 0, best_sticks))
+		no_cross_sticks += best_sticks #List[Dict]
+		word_set = disjoint_set.from_iterable(chain.from_iterable([(i, j) for j in range(len(self._l[i]))] for i in range(4)))
+		for d in no_cross_sticks:
+			if len(d) <= 1:
+				continue
+			i = iter(d.items())
+			s, index = next(i) #consume
+			s = r_mapping[s]
+			for s_i, index_i in i:
+				word_set.union((s, index), (r_mapping[s_i], index_i))
+		self._big_anchor_state = {'word_set': word_set}
+		return self._big_anchor_state
+	@classmethod
+	def _big_anchor_concat_james_word_counter(cls, strings, word_to_count):
+		count = 0
+		for i in range(0, len(strings)):
+			for word in strings[i]:
+				if word == word_to_count:
+					count += 1
+		return count
+	@classmethod
+	def _big_anchor_concat_james_helper(cls, strings, sticks, shifts = [0, 0, 0, 0], shifts_diff = [0, 0, 0, 0], mode = 'right'):
+		strings_len = [len(s) for s in strings]
+		
+		now_score = -np.inf
+		max_score = -np.inf
+		left_max_score = 0
+		right_max_score = 0
+
+		now_sticks = []
+		best_sticks = sticks
+		left_best_sticks = []
+		right_best_sticks = []
+		find_stick = False
+		
+		if mode == "right":
+			for i in range(0, 2):
+				for j in range(0, len(sticks[i])):
+					for k in sticks[i][j].keys():
+						sticks[i][j][k] -= shifts_diff[k]
+		
+		top_stick_index = 0
+		for top_stick in sticks[0]:
+			string2_index = 0
+			for string2_word in strings[2]:
+				if string2_word == strings[0][top_stick[0]]:
+					find_stick = True
+					
+					is_stick = False
+					left_strings = [strings[0][0:top_stick[0]], strings[1][0:top_stick[1]], strings[2][0:string2_index]]
+					right_strings = [strings[0][top_stick[0] + 1:strings_len[0]],
+												strings[1][top_stick[1] + 1:strings_len[1]],
+												strings[2][string2_index + 1:strings_len[2]]]
+					left_sticks = [sticks[0][0:top_stick_index]]
+					right_sticks = [sticks[0][top_stick_index + 1:len(sticks[0])]]
+					
+					bottom_stick_index = 0
+					for bottom_stick in sticks[1]:
+						if bottom_stick[2] == string2_index:
+							is_stick = True
+							break
+						bottom_stick_index += 1
+
+					#the word is a stick
+					if is_stick == True:
+						left_strings.append(strings[3][0:sticks[1][bottom_stick_index][3]])
+						right_strings.append(strings[3][sticks[1][bottom_stick_index][3] + 1:strings_len[3]])
+						left_sticks.append(sticks[1][0:bottom_stick_index])
+						right_sticks.append(sticks[1][bottom_stick_index + 1:len(sticks[1])])
+						
+						left_shifts = shifts
+						right_shifts = [shifts[0] + 1 + top_stick[0], shifts[1] + 1 + top_stick[1], shifts[2] + 1 + string2_index, shifts[3] + 1 + sticks[1][bottom_stick_index][3]]
+						left_shifts_diff = [0, 0, 0, 0]
+						right_shifts_diff = []
+						for i in range(0, 4):
+							right_shifts_diff.append(right_shifts[i] - shifts[i])
+						
+						left_max_score, left_best_sticks = cls._big_anchor_concat_james_helper(left_strings, left_sticks, left_shifts, left_shifts_diff, "left")
+						right_max_score, right_best_sticks = cls._big_anchor_concat_james_helper(right_strings, right_sticks, right_shifts, right_shifts_diff, "right")
+						now_score = left_max_score + 4 + right_max_score
+						if now_score > max_score:
+							adjust_top_stick = {k: i + shifts[k] for k, i in top_stick.items()}
+							adjust_bottom_stick = {k: i + shifts[k] for k, i in sticks[1][bottom_stick_index].items()}
+							
+							max_score = now_score
+							best_sticks = [left_best_sticks[0] + [{**adjust_top_stick, **adjust_bottom_stick}] + right_best_sticks[0], left_best_sticks[1] + right_best_sticks[1]]
+					
+					#the word is not a stick
+					elif is_stick == False and strings_len[3] > 0:
+						string3_to_count_index_start = 0
+						string3_to_count_index_finish = strings_len[3] - 1
+						bottom_stick_index_before = -1
+						bottom_stick_index_after = -1
+						for bottom_stick in sticks[1]:
+							if bottom_stick[2] < string2_index:
+								string3_to_count_index_start = bottom_stick[3] + 1
+								bottom_stick_index_before += 1
+							else:
+								string3_to_count_index_finish = bottom_stick[3] - 1
+								bottom_stick_index_after = bottom_stick_index_before + 1
+								break
+							
+						string3_start_right_index = string3_to_count_index_finish + 1
+						for string3_middle_index in range(string3_to_count_index_start, string3_to_count_index_finish + 1):
+							if cls._big_anchor_concat_james_word_counter(left_strings, strings[3][string3_middle_index]) < cls._big_anchor_concat_james_word_counter(right_strings, strings[3][string3_middle_index]):
+								string3_start_right_index = string3_middle_index
+								break
+						
+						left_strings.append(strings[3][0:string3_start_right_index])
+						right_strings.append(strings[3][string3_start_right_index:strings_len[3]])
+						left_sticks.append(sticks[1][0:bottom_stick_index_before + 1])
+						right_sticks.append(sticks[1][bottom_stick_index_after:len(sticks[1])])
+						
+						left_shifts = shifts
+						right_shifts = [shifts[0] + 1 + top_stick[0], shifts[1] + 1 + top_stick[1], shifts[2] + 1 + string2_index, shifts[3] + 1 + string3_start_right_index-1]
+						left_shifts_diff = [0, 0, 0, 0]
+						right_shifts_diff = []
+						for i in range(0, 4):
+							right_shifts_diff.append(right_shifts[i] - shifts[i])
+						
+						left_max_score, left_best_sticks = cls._big_anchor_concat_james_helper(left_strings, left_sticks, left_shifts, left_shifts_diff, "left")
+						right_max_score, right_best_sticks = cls._big_anchor_concat_james_helper(right_strings, right_sticks, right_shifts, right_shifts_diff, "right")
+						
+						now_score = left_max_score + 3 + right_max_score
+						if now_score > max_score:
+							adjust_top_stick = {k: i + shifts[k] for k, i in top_stick.items()}
+							
+							max_score = now_score
+							best_sticks = [left_best_sticks[0] + [{**adjust_top_stick, 2: string2_index + shifts[2]}] + right_best_sticks[0], left_best_sticks[1] + right_best_sticks[1]]
+
+				string2_index += 1
+			
+			string3_index = 0
+			for string3_word in strings[3]:
+				if string3_word == strings[0][top_stick[0]]:
+					find_stick = True
+					is_stick = False
+					left_strings = [strings[0][0:top_stick[0]], strings[1][0:top_stick[1]], strings[3][0:string3_index]]
+					right_strings = [strings[0][top_stick[0] + 1:strings_len[0]],
+												strings[1][top_stick[1] + 1:strings_len[1]],
+												strings[3][string3_index + 1:strings_len[3]]]
+					left_sticks = [sticks[0][0:top_stick_index]]
+					right_sticks = [sticks[0][top_stick_index + 1:len(sticks[0])]]
+					
+					for bottom_stick in sticks[1]:
+						if bottom_stick[3] == string3_index:
+							is_stick = True
+							break
+					
+					#the word is not a stick
+					if is_stick == False and strings_len[2] > 0:
+						
+						string2_to_count_index_start = 0
+						string2_to_count_index_finish = strings_len[2] - 1
+						bottom_stick_index_before = -1
+						bottom_stick_index_after = -1
+						for bottom_stick in sticks[1]:
+							if bottom_stick[3] < string3_index:
+								string2_to_count_index_start = bottom_stick[2] + 1
+								bottom_stick_index_before += 1
+							else:
+								string3_to_count_index_finish = bottom_stick[2] - 1 #???
+								bottom_stick_index_after = bottom_stick_index_before + 1
+								break
+							
+						string2_start_right_index = string2_to_count_index_finish + 1
+						for string2_middle_index in range(string2_to_count_index_start, string2_to_count_index_finish + 1):
+							if cls._big_anchor_concat_james_word_counter(left_strings, strings[2][string2_middle_index]) < cls._big_anchor_concat_james_word_counter(right_strings, strings[2][string2_middle_index]):
+								string2_start_right_index = string2_middle_index
+								break
+						
+						left_strings.insert(2, strings[2][0:string2_start_right_index])
+						right_strings.insert(2, strings[2][string2_start_right_index:strings_len[2]])
+						left_sticks.insert(2, sticks[1][0:bottom_stick_index_before + 1])
+						right_sticks.insert(2, sticks[1][bottom_stick_index_after:len(sticks[1])])
+						
+						left_shifts = shifts
+						right_shifts = [shifts[0] + 1 + top_stick[0], shifts[1] + 1 + top_stick[1], shifts[2] + 1 + string2_start_right_index-1, shifts[3] + 1 + string3_index]
+						left_shifts_diff = [0, 0, 0, 0]
+						right_shifts_diff = []
+						for i in range(0, 4):
+							right_shifts_diff.append(right_shifts[i] - shifts[i])
+						
+						left_max_score, left_best_sticks = cls._big_anchor_concat_james_helper(left_strings, left_sticks, left_shifts, left_shifts_diff, "left")
+						right_max_score, right_best_sticks = cls._big_anchor_concat_james_helper(right_strings, right_sticks, right_shifts, right_shifts_diff, "right")
+						
+						now_score = left_max_score + 3 + right_max_score
+						if now_score > max_score:
+							adjust_top_stick = {k: i + shifts[k] for k, i in top_stick.items()}
+							max_score = now_score
+							best_sticks = [left_best_sticks[0] + [{**adjust_top_stick, 3: string3_index + shifts[3]}] + right_best_sticks[0], left_best_sticks[1] + right_best_sticks[1]]
+
+				string3_index += 1
+			
+			top_stick_index += 1
+		
+		#base case
+		if find_stick == False:
+			bottom_stick_number = len(sticks[1])
+			max_score = -1*(strings_len[0] + strings_len[1] + strings_len[2] + strings_len[3]) + bottom_stick_number * 2 * 2
+		
+		if mode == "right":
+			for i in range(0,2):
+				for j in range(0,len(sticks[i])):
+					for k in sticks[i][j].keys():
+						sticks[i][j][k] += shifts_diff[k]
+
+		return max_score, best_sticks
+	def print_big_anchor(self, word_set: disjoint_set = None):
 		"""
 		test function to represent the solution
 		"""
@@ -178,11 +487,11 @@ class StringAlign():
 				str_list[i][id_dict[index[(i, j)]]] = word
 		print('\n'.join([' '.join(s) for s in str_list]))
 	@classmethod
-	def compare(cls, l1 : List[str], l2 : List[str], param : Param):
+	def compare(cls, l1: List[str], l2: List[str], param: Param):
 		anchors = cls._anchors(l1, l2)
 		return cls._compare_detail(l1, l2, param, anchors)
 	@staticmethod
-	def _anchors(l1 : List[str], l2 : List[str]) -> List[AnchorType]:
+	def _anchors(l1: List[str], l2: List[str]) -> List[AnchorType]:
 		"""
 		input two 'string' which is splitted into list of words.
 		output all pairs of indexs which means:
@@ -197,7 +506,7 @@ class StringAlign():
 			sol.extend([np.array(i) for i in product(i1, i2)])
 		return sol
 	@classmethod
-	def _compare_split(cls, l1 : List[str], l2 : List[str], param : Param, anchors : List[AnchorType], now_anchor : AnchorType) -> Tuple[ScoreType, List[AnchorType]]:
+	def _compare_split(cls, l1: List[str], l2: List[str], param: Param, anchors: List[AnchorType], now_anchor: AnchorType) -> Tuple[ScoreType, List[AnchorType]]:
 		"""
 		recursion function that always called by _compare_detail
 		deal with the step: considering an anchor being fixed, calculate the max possible point(score) it could be.
@@ -229,7 +538,7 @@ class StringAlign():
 		sol_simi = param.merge_point(left_child, right_child, left_ans, right_ans)
 		return cls.Ans(sol_simi, sol_anchor)
 	@classmethod
-	def _compare_detail(cls, l1 : List[str], l2 : List[str], param : Param, anchors : List[AnchorType]) -> Tuple[ScoreType, List[AnchorType]]:
+	def _compare_detail(cls, l1: List[str], l2: List[str], param: Param, anchors: List[AnchorType]) -> Tuple[ScoreType, List[AnchorType]]:
 		"""
 		recursion function that called from outside (i.e., it is the entry of recursion) or by _compare_split
 		deal with the step: given all possible anchors, I want to know which anchor, when fixing it first, can get the highest point(score)
@@ -276,5 +585,9 @@ if __name__ == '__main__':
 
 	S.evaluate(p)
 	print(S)
-	S.big_anchor_concat_heuristic(p)
+	x = S.big_anchor_concat_james()
+	#x = S.big_anchor_concat_heuristic(p)
+	x = x['word_set']
 	S.print_big_anchor()
+	print(x.sets())
+	print(x.copy().sets())
